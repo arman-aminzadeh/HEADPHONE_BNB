58ea446b7d03eb8bf6f3b3f3cfa9221838fc6198
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-02 00:21:05 +0100

    Creating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,18 @@
     create.product!(
         name: Faker::name
         description: Faker::Lorem.sentence
-        address: Faker::Address.full_address
-        price_per_day: Faker::Commerce.price
-        
+        address: 
+        price_per_day: 7$
+        picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/b/l/black_losangeles_studio_packshot_2.webp',
     )
+
+"Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
