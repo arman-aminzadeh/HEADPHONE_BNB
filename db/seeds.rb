753b3ee5bc33abcf66982577387abd0b459738fd
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-01 23:46:20 +0100

    Creating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+12.times do
+    create.product!(
+        name: Faker::name
+        description: Faker::Lorem.sentence
+        address: Faker::Address.full_address
+        price_per_day: Faker::Commerce.price
+        
+    )
