69c7812e2843b3015e5fd275b02b1e9e022e7f80
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-03 19:00:08 +0100

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,78 +11,88 @@ Product.create!(
     name: "Black Losangeles",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 7$
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/b/l/black_losangeles_studio_packshot_2.webp'
+    price_per_day: 7,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/b/l/black_losangeles_studio_packshot_2.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "New York",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 5$
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30280_1_1.webp'
+    price_per_day: 5,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30280_1_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Miami",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 3$
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/4/44259_1.webp'
+    price_per_day: 3,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/4/44259_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Stockholm Plus",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 3.5$,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/0/40407_1.webp'
+    price_per_day: 3.5,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/0/40407_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Atens",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 4.2$,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/5/35327-1_1.webp'
+    price_per_day: 4.2,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/5/35327-1_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Seoul",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 8$,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/0/0/0006_midnight_black_angle_1.webp'
+    price_per_day: 8,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/0/0/0006_midnight_black_angle_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Berlin",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 6$,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/6/26808_1.webp'
+    price_per_day: 6,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/6/26808_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Madrid",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 6.5$,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30268_1.webp'
+    price_per_day: 6.5,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30268_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Miami Crystal Edition",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 10$,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/5/45683_1_1.webp'
+    price_per_day: 10,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/5/45683_1_1.webp',
+    user_id: 1
 )
 
 Product.create!(
     name: "Boston",
     description: Faker::Lorem.sentence,
     address: Faker::Address.full_address,
-    price_per_day: 2.8$,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/1/21315_1.webp'
+    price_per_day: 2.8,
+    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/1/21315_1.webp',
+    user_id: 1
 )
