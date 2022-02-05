2616fb833a29cf6a6c02a14ebfcc5feb94f76d11
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-05 14:03:13 +0100

    deleting first line in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  has_many :bookings, foreign_key: "reference_id"
+  
   def index
     @products = policy_scope(Product).order(created_at: :desc)
   end
