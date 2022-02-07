6ba88c360c061a7a1d62f3b81f96c64909bd6fd9
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-08 00:40:58 +0100

    booking controller/view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,18 @@
 class BookingsController < ApplicationController
-  
+
+    def new
+        @booking = Booking.new
+    end
+    
+    def create
+        
+    end
+    
+    def update
+
+    end
+    
+    def destroy
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :products, only: [ :index, :show, :edit, :destroy, :update ]
   resources :users do 
     resources :products, only: [ :create, :new, :destroy ]
+    resources :bookings, only: [ :create, :new ]
   end
-
+  resources :bookings, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
