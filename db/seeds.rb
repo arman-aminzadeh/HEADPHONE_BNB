6321f17bc6e8ebb6bdafbd1c085a5efe22e24fde
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-02-08 20:33:57 +0100

    done and test Map Module

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,93 +1,48 @@
 require "faker"
 
+# DONT DELETE
+pictures = %w[
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/b/l/black_losangeles_studio_packshot_2.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30280_1_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30280_1_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/0/40407_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/5/35327-1_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/0/0/0006_midnight_black_angle_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/6/26808_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30268_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/5/45683_1_1.webp
+  https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/1/21315_1.webp
+              ]
+# DONT DELETE
+
+# pictures = %w[
+#   black_los_angeles_otdtvm
+#   new_york_tccs5o
+#   miami_sirijp
+#   stockholm_plus_l2t5d9
+#   athens_auanme
+#   seoul_upxhcs
+#   berlin_iyowt
+#   madrid_osdre7
+#   miami_crystal_edition_en7fwn
+#   boston_iuvtzv
+#
+# ]
+puts "Start Seeding"
+
 Product.destroy_all
 
-Product.create!(
-    name: "Black Losangeles",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 7.00,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/b/l/black_losangeles_studio_packshot_2.webp',
-    user_id: 1
-)
+10.times do |i|
+  Product.create!(
+    name:          Faker::Company.name,
+    picture_url:   pictures[i],
+    address:       Faker::Address.full_address,
+    description:   Faker::Lorem.paragraph,
+    price_per_day: rand(1..10).to_f,
+    latitude:      Faker::Address.latitude,
+    longitude:     Faker::Address.longitude,
+    user_id:       1
+  )
+end
 
-Product.create!(
-    name: "New York",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 5.00,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30280_1_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Miami",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 3.00,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/4/44259_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Stockholm Plus",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 3.50,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/0/40407_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Atens",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 4.20,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/5/35327-1_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Seoul",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 8.88,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/0/0/0006_midnight_black_angle_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Berlin",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 6.66,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/6/26808_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Madrid",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 6.50,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/3/0/30268_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Miami Crystal Edition",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 10,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/4/5/45683_1_1.webp',
-    user_id: 1
-)
-
-Product.create!(
-    name: "Boston",
-    description: Faker::Lorem.sentence,
-    address: Faker::Address.full_address,
-    price_per_day: 2.80,
-    picture_url: 'https://www.urbanista.com/media/catalog/product/cache/6c07725d11cf11164242a71cef72688e/2/1/21315_1.webp',
-    user_id: 1
-)
+puts "Seeded!"
