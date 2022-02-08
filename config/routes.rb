0e6cea0e64ce754629a572a507cdd40cec64bfb6
Sebastian <65852150+JANKEE92PL@users.noreply.github.com>
2022-02-08 21:16:10 +0100

    Update routes.rb
    update the routes with Team

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :products, only: [ :index, :show, :edit, :destroy, :update ] do 
+  resources :products, only: [ :index, :show ] do 
     resources :bookings, only: [ :create, :new ]
   end
+  
   resources :bookings, only: [ :destroy, :show ]
   resources :users do 
-    resources :products, only: [ :create, :new ]
+    resources :products, only: [ :create, :new, :edit, :destroy, :update]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
