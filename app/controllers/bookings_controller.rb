c2b404e533b8dd8a5642fe1ebf4f4a45850e887b
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-08 12:29:02 +0100

    Booking controller/view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,9 +2,14 @@ class BookingsController < ApplicationController
 
     def new
         @booking = Booking.new
+        @user = current_user
+        authorize @booking
     end
     
     def create
+        raise
+        @booking = Booking.new(booking_params)
+        @booking.user = @user
         
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :products, only: [ :index, :show, :edit, :destroy, :update ]
+
+  resources :bookings, only: [ :create, :new, :destroy ]
+  
   resources :users do 
     resources :products, only: [ :create, :new, :destroy ]
-    resources :bookings, only: [ :create, :new ]
   end
-  resources :bookings, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
