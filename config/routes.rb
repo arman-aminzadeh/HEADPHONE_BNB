84ec14d48f288354d55061965b6de9971d47561e
Dennis Neumann <90464044+dennisneumann@users.noreply.github.com>
2022-02-08 20:24:21 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :products, only: [ :index, :show, :edit, :destroy, :create, :new ] do
-    resources :bookings, only: [ :create ]
-  end
   root to: 'pages#home'
+  resources :products, only: [ :index, :show, :edit, :destroy, :update ]
+  resources :users do 
+    resources :products, only: [ :create, :new, :destroy ]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
