9c7d640260ca76055863d654df4187c74f8f52e5
Dennis Neumann <mail@dennis-neumann.com>
2022-02-12 11:15:07 +0100

    Fixing the routes for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :products, only: [ :index, :show ] do
+  resources :products, only: [ :index, :show, :edit, :destroy, :update ] do
     resources :bookings, only: [ :create, :new ]
   end
 
   resources :bookings, only: [ :destroy, :show ]
   resources :users do
-    resources :products, only: [ :create, :new, :edit, :destroy, :update]
+    resources :products, only: [ :create, :new]
   end
 end
