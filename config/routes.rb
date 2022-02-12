d7aa84838c1320a37ee19ac12b4e9cac67a9e957
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-12 15:21:54 +0100

    Adding review

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :products, only: [ :index, :show ] do
     resources :bookings, only: [ :create, :new ]
-    resources :reviews, only: [ :create ]
+    resources :reviews, only: [ :new, :create ]
   end
 
   resources :bookings, only: [ :destroy, :show ]
